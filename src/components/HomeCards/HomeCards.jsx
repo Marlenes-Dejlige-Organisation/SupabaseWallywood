31ac5f3516e3249fab45bbd6fc0738551c5d9b0d
MarlaRey <hejmarlenehej@gmail.com>
2024-01-19 16:39:27 +0100

    home og about på plads

--- src/components/HomeCards/HomeCards.jsx
+++ src/components/HomeCards/HomeCards.jsx
@@ -2,40 +2,43 @@
 import React, { useEffect, useState } from 'react';
 import styles from './HomeCards.module.scss';
 
-
 export const HomeCards = () => {
-    const [allPosters, setAllPosters] = useState([]);
-    const [latestPosters, setLatestPosters] = useState([]);
-  
-    useEffect(() => {
-      // Fetch all posters
-      fetch('http://localhost:3000/posters')
-        .then((response) => response.json())
-        .then((data) => setAllPosters(data))
-        .catch((error) => console.error('Error fetching posters:', error));
-    }, []);
-  
-    useEffect(() => {
-      // Filter the two latest posters from all posters
-      const sortedPosters = [...allPosters].sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-      const latestTwoPosters = sortedPosters.slice(0, 2);
-      setLatestPosters(latestTwoPosters);
-    }, [allPosters]);
-  
-    return (
-      <div className={styles.homecards}>
-        {latestPosters.map((poster) => (
-          <div key={poster.id} className="card">
+  const [allPosters, setAllPosters] = useState([]);
+  const [latestPosters, setLatestPosters] = useState([]);
+
+  useEffect(() => {
+    // Fetch all posters
+    fetch('http://localhost:3000/posters')
+      .then((response) => response.json())
+      .then((data) => setAllPosters(data))
+      .catch((error) => console.error('Error fetching posters:', error));
+  }, []);
+
+  useEffect(() => {
+    // Filter the two latest posters from all posters
+    const sortedPosters = [...allPosters].sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    const latestTwoPosters = sortedPosters.slice(0, 2);
+    setLatestPosters(latestTwoPosters);
+  }, [allPosters]);
+
+  return (
+    <div className={styles.homecards}>
+      {latestPosters.map((poster) => (
+        <div key={poster.id} className={styles.card}>
+          <div>
             <img src={poster.image} alt={poster.name} />
+          </div>
+          <div>
             <h4>{poster.name}</h4>
             <p>{poster.slug}</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, doloremque et! In tota natus! Beatae ratione nemo dolore dolores culpa, aperiam excepturi ex!</p>
             {poster.genres && (
-              <p>{poster.genres.map((genre) => genre.title).join(', ')}</p>
+              <p>Genre: {poster.genres.map((genre) => genre.title).join(', ')}</p>
             )}
+            <button>Læs mere</button>
           </div>
-        ))}
-      </div>
-    );
-  };
-
-
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,17 +1,24 @@
 import { ContentWrapper } from "../../components/ContentWrapper/ContentWrapper"
+import Curtains from '../../assets/images/Frame 4.png'
+import styles from './About.module.scss';
 
 
 
-export const About =() =>{
-    return(
+export const About = () => {
+    return (
         <>
-   
-        <ContentWrapper title="Om os">
-            <img src="" alt="billede af en stjerne" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia atque nulla harum quidem dolore quasi unde. Dolorum deserunt voluptatum natus aut suscipit. Earum iste ratione eum molestias explicabo iure quisquam.</p>
-            <p>React blev udviklet af Facebook og er designet til at gøre det lettere at opbygge effektive og genanvendelige brugergrænseflader i webapplikationer.</p>
-            <p>Selvom React i sig selv ikke er et framework, bruges det ofte sammen med andre værktøjer og biblioteker som Redux for tilstandsstyring eller React Router for navigering. Sammen med disse kan React danne kernen i udviklingen af moderne, dynamiske webapplikationer.</p>
-        </ContentWrapper>
+
+            <ContentWrapper title="Om os" >
+                <div className={styles.grid}>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia atque nulla harum quidem dolore quasi unde. Dolorum deserunt voluptatum natus aut suscipit. Earum iste ratione eum molestias explicabo iure quisquam.</p>
+                    <p>React blev udviklet af Facebook og er designet til at gøre det lettere at opbygge effektive og genanvendelige brugergrænseflader i webapplikationer.</p>
+                    <p>Selvom React i sig selv ikke er et framework, bruges det ofte sammen med andre værktøjer og biblioteker som Redux for tilstandsstyring eller React Router for navigering. Sammen med disse kan React danne kernen i udviklingen af moderne, dynamiske webapplikationer.</p>
+                </div><div>
+                    <img src={Curtains} alt="gardiner" />
+                </div>
+                </div>
+            </ContentWrapper>
         </>
     )
 }
