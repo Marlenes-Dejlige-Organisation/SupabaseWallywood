4f88126b10cbf8073b2c7f8513a21ecd16458f96
MarlaRey <hejmarlenehej@gmail.com>
2024-01-24 22:56:23 +0100

    genrene er ude

--- src/components/Poster-routing/GenreNav.jsx
+++ src/components/Poster-routing/GenreNav.jsx
@@ -1,11 +1,33 @@
+import axios from "axios"
+import { NavLink } from "react-router-dom"
+import {useEffect, useState} from "react"
+
 export const GenreNav = () => {
+const [apiData, setApiData] = useState([])
+const getData = async () =>{
+    const endpoint = `http://localhost:3000/genre`
+    const result = await axios.get (endpoint)
+    setApiData(result.data);
+}
+useEffect(()=>{
+    getData()
+}, [setApiData])
+
     return (
     <>
     
     <ul>
     <h2>Genre</h2>
-        <li>Drama</li>
+    {apiData && apiData.map(item =>{
+        return (
+            <li key={item.id}>
+            <NavLink to={item.slug}>{item.title}</NavLink>
+            </li>
+        )
+    })}
+        <li><NavLink to="/plakater/drama">Drama</NavLink></li>
     </ul>
     </>
+
     )
 }
