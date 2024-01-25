c0062c78d669b78b5e49ba2a01154cd86c6439c9
MarlaRey <hejmarlenehej@gmail.com>
2024-01-25 14:03:10 +0100

    plakater-side styling i alle led, tjeck

--- src/components/Poster-routing/PosterDetails.jsx
+++ src/components/Poster-routing/PosterDetails.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./PosterDetails.scss"; // Import your SCSS file
 
+
 export const PosterDetails = () => {
   const [apiData, setApiData] = useState({});
   const { poster } = useParams();
@@ -11,7 +12,7 @@ export const PosterDetails = () => {
   const getData = async () => {
     const endpoint = `http://localhost:3000/posters/${poster}`;
     const result = await axios.get(endpoint);
-    console.log(result.data);
+   
     setApiData(result.data);
   };
 
@@ -20,12 +21,20 @@ export const PosterDetails = () => {
   }, [poster]);
 
   return (
-    <div className="poster-details-container">
+    <div className="poster-details-container" >
+        
       <img src={apiData.image} alt={apiData.name} />
       <div>
-        <h2>{apiData.name}</h2>
-        <p>{apiData.description}</p>
-        <p>Pris: {apiData.price}</p>
+        <h3>{apiData.name}</h3>
+        <p dangerouslySetInnerHTML={{__html: apiData.description}}></p>
+        <p>Størrelse: {apiData.width} * {apiData.height} cm.</p>
+        <p >Varenummer (SKU): {apiData.id}</p>
+        <h4>Kr. {apiData.price},00</h4>
+        <input type="number" placeholder="0"/>
+        <button>Læg i kurv</button>
+        <div className="stock">
+        <p>{apiData.stock} på lager</p>
+        </div>
       </div>
     </div>
   );
