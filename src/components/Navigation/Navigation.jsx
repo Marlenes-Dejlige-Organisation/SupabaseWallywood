e5bea1274b01df01734608edc0c8e9eee719db9e
MarlaRey <hejmarlenehej@gmail.com>
2024-01-29 12:50:57 +0100

    nav active-styling done

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,17 +3,19 @@ import { NavLink } from 'react-router-dom';
 import vogn from '../../assets/images/shopping_cart_FILL0_wght400_GRAD0_opsz24.png';
 
 export const Navigation = () => {
+  const activeLink = "font-bold";
+ 
   return (
     <>
       <div className={styles.ikon}><img src={vogn} alt="vogn" /></div>
       <nav className={styles.navWrapper}>
         <h1>WALLYWOOD</h1>
         <ul>
-          <li><NavLink exact to="/" className={styles.NavLink} activeclassname={styles.activeNavLink}>HOME</NavLink></li>
-          <li><NavLink to="/plakater" className={styles.NavLink} activeclassname={styles.activeNavLink}>PLAKATER</NavLink></li>
-          <li><NavLink to="/about" className={styles.NavLink} activeclassname={styles.activeNavLink}>OM OS</NavLink></li>
-          <li><NavLink to="/contact" className={styles.NavLink} activeclassname={styles.activeNavLink}>KONTAKT</NavLink></li>
-          <li><NavLink to="/login" className={styles.NavLink} activeclassname={styles.activeNavLink}>LOGIN</NavLink></li>
+          <li><NavLink exact to="/" className={({isActive})=> isActive ? styles.activeNavLink : styles.NavLink} >HOME</NavLink></li>
+          <li><NavLink to="/plakater" className={({isActive})=> isActive ? styles.activeNavLink : styles.NavLink}>PLAKATER</NavLink></li>
+          <li><NavLink to="/about" className={({isActive})=> isActive ? styles.activeNavLink : styles.NavLink}>OM OS</NavLink></li>
+          <li><NavLink to="/contact" className={({isActive})=> isActive ? styles.activeNavLink : styles.NavLink}>KONTAKT</NavLink></li>
+          <li><NavLink to="/login" className={({isActive})=> isActive ? styles.activeNavLink : styles.NavLink}>LOGIN</NavLink></li>
         </ul>
       </nav>
     </>
