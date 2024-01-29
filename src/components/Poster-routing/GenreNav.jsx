360a9305870a557806f37ffc49b31d8a6182d7eb
MarlaRey <hejmarlenehej@gmail.com>
2024-01-29 13:25:20 +0100

    autoload af plakater på plakater

--- src/components/Poster-routing/GenreNav.jsx
+++ src/components/Poster-routing/GenreNav.jsx
@@ -1,33 +1,36 @@
-import axios from "axios"
-import { NavLink } from "react-router-dom"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import styles from '../Navigation/Navigation.module.scss';
 
-export const GenreNav = () => {
-    const [apiData, setApiData] = useState([])
+export const GenreNav = ({ onDefaultGenreSelected }) => {
+    const [apiData, setApiData] = useState([]);
+    
     const getData = async () => {
-        const endpoint = `http://localhost:3000/genre`
-        const result = await axios.get(endpoint)
+        const endpoint = `http://localhost:3000/genre`;
+        const result = await axios.get(endpoint);
         setApiData(result.data);
-    }
+        
+        // Hvis der er data og en callback er tilgængelig, vælg den første genre som standard
+        if (result.data.length > 0 && onDefaultGenreSelected) {
+            onDefaultGenreSelected(result.data[0].slug);
+        }
+    };
+
     useEffect(() => {
-        getData()
-    }, [setApiData])
+        getData();
+    }, []);
 
     return (
         <>
-
             <ul>
                 <h3>Genre</h3>
-                {apiData && apiData.map(item => {
-                    return (
-                        <li key={item.id}>
-                            <NavLink to={item.slug}>{item.title}</NavLink>
-                        </li>
-                    )
-                })}
-                <li><NavLink to="/plakater/drama">Drama</NavLink></li>
+                {apiData && apiData.map(item => (
+                    <li key={item.id}>
+                        <NavLink to={item.slug} className={({ isActive }) => isActive ? styles.activeNavLink : styles.NavLink}>{item.title}</NavLink>
+                    </li>
+                ))}
             </ul>
         </>
-
-    )
-}
+    );
+};
--- src/pages/Plakater/Plakater.jsx
+++ src/pages/Plakater/Plakater.jsx
@@ -1,17 +1,25 @@
+// Plakater.jsx
+import React, { useState } from 'react';
 import styles from './Plakater.module.scss';
 import { GenreNav } from '../../components/Poster-routing/GenreNav';
 import { ContentWrapper } from "../../components/ContentWrapper/ContentWrapper";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { PosterList } from '../../components/Poster-routing/PosterList';
 
 export const Plakater = () => {
+  const [defaultGenre, setDefaultGenre] = useState(null);
+  const navigate = useNavigate();
 
-
+  const handleDefaultGenreSelected = (genre) => {
+    setDefaultGenre(genre);
+    navigate(`/plakater/${genre}`);
+  };
 
   return (
     <ContentWrapper title="Plakater">
       <div className={styles.pageContainer}>
         <div className={styles.ListContainer}>
-          <GenreNav />
+          <GenreNav onDefaultGenreSelected={handleDefaultGenreSelected} />
         </div>
         <div className={styles.posterContainer}>
           <Outlet />
