7b84902a829c46199d2b12c1c7077e56cc69e77d
MarlaRey <hejmarlenehej@gmail.com>
2024-02-05 13:09:20 +0100

    mediaquiries på plads, pånær plakatsiden

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,7 +10,7 @@ export const About = () => {
 
             <ContentWrapper title="Om os" >
                 <div className={styles.grid}>
-                <div>
+                <div className={styles.content}>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia atque nulla harum quidem dolore quasi unde. Dolorum deserunt voluptatum natus aut suscipit. Earum iste ratione eum molestias explicabo iure quisquam.</p>
                     <p>React blev udviklet af Facebook og er designet til at gøre det lettere at opbygge effektive og genanvendelige brugergrænseflader i webapplikationer.</p>
                     <p>Selvom React i sig selv ikke er et framework, bruges det ofte sammen med andre værktøjer og biblioteker som Redux for tilstandsstyring eller React Router for navigering. Sammen med disse kan React danne kernen i udviklingen af moderne, dynamiske webapplikationer.</p>
